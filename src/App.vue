a9b5e57a766e2eabb02b782264d80de1b01855c2
glebdyakov <glebdyakov2000@gmail.com>
2023-12-26 14:17:32 +0300

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <nav />
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,6 @@
+<template>
+  <main />
+</template>
+
+<script setup lang="ts">
+</script>
