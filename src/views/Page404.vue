bbf3bd7164da74fbbbecee47635f4527b1d6f328
glebdyakov <glebdyakov2000@gmail.com>
2024-01-19 16:54:32 +0300

    адаптив: adaptive 404 page

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -1,11 +1,11 @@
 <template>
     <div class="flex items-center justify-center gap-[137px] h-screen">
-        <div>
-            <p class="page404--title text-[#378A6F] text-[80px] font-medium mb-[14px]">Ошибка 404</p>
-            <p class="page404--subtitle mb-[30px]">Ой, кажется все зайцы разбежались</p>
+        <div class="flex flex-col items-center">
+            <p class="page404--title text-[#378A6F] text-[40px] sm:text-[80px] font-medium mb-[14px]">Ошибка 404</p>
+            <p class="page404--subtitle mb-[30px] text-center">Ой, кажется все зайцы разбежались</p>
             <button class="btn btn--minimized w-[225px] h-[54px] flex justify-center items-center whitespace-nowrap">На главную</button>
         </div>
-        <div>
+        <div class="hidden md:block">
             <div class="w-[525.77px] h-[525.96px] bg-[#6CC4A7] absolute z-[-1] translate-y-[38px] rotate-[15deg]" />
             <img width="548px" height="667px" src="@/assets/img/404.png" alt="404">
         </div>
