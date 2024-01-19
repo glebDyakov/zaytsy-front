e232c5efa377f51af2e3293cd38e381423fc1d9e
glebdyakov <glebdyakov2000@gmail.com>
2024-01-19 21:58:36 +0300

    адаптив: add tooltip for partner info

--- src/views/Partners.vue
+++ src/views/Partners.vue
@@ -7,72 +7,92 @@
         <div class="partners">
             <div class="partner w-[158px] h-[110px] md:w-[228px] md:h-[176px] flex flex-col">
                 <div class="w-full flex justify-between md:justify-end px-[12px]">
-                    <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
-                    <img src="@/assets/icons/crown.svg" alt="best" />
-                </div>
-                <img src="@/assets/icons/Acura.svg" class="w-[88px] h-[56px] md:w-[150px] md:h-[112px]" alt="Acura" />
-            </div>
-            <div class="partner w-[158px] h-[110px] md:w-[228px] md:h-[176px] flex flex-col">
-                <div class="w-full flex best--wrap">
-                    <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
-                    <img src="@/assets/icons/crown.svg" alt="best" />
-                </div>
-                <img src="@/assets/icons/Acura.svg" class="w-[88px] h-[56px] md:w-[150px] md:h-[112px]" alt="Acura" />
-            </div>
-            <div class="partner w-[158px] h-[110px] md:w-[228px] md:h-[176px] flex flex-col">
-                <div class="w-full flex justify-between md:justify-end px-[12px]">
-                    <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    <Tippy theme="dark" :content="content">
+                        <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    </Tippy>
                     <img src="@/assets/icons/crown.svg" alt="best" />
                 </div>
                 <img src="@/assets/icons/Acura.svg" class="w-[88px] h-[56px] md:w-[150px] md:h-[112px]" alt="Acura" />
             </div>
             <div class="partner w-[158px] h-[110px] md:w-[228px] md:h-[176px] flex flex-col">
                 <div class="w-full flex justify-between md:justify-end px-[12px]">
-                    <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    <Tippy theme="dark" :content="content">
+                        <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    </Tippy>
                     <img src="@/assets/icons/crown.svg" alt="best" />
                 </div>
                 <img src="@/assets/icons/Acura.svg" class="w-[88px] h-[56px] md:w-[150px] md:h-[112px]" alt="Acura" />
             </div>
             <div class="partner w-[158px] h-[110px] md:w-[228px] md:h-[176px] flex flex-col">
                 <div class="w-full flex justify-between md:justify-end px-[12px]">
-                    <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    <Tippy theme="dark" :content="content">
+                        <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    </Tippy>
+                    <img src="@/assets/icons/crown.svg" alt="best" />
+                </div>
+                <img src="@/assets/icons/Acura.svg" class="w-[88px] h-[56px] md:w-[150px] md:h-[112px]" alt="Acura" />
+            </div>
+            <div class="partner w-[158px] h-[110px] md:w-[228px] md:h-[176px] flex flex-col">
+                <div class="w-full flex justify-between md:justify-end px-[12px]">
+                    <Tippy theme="dark" :content="content">
+                        <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    </Tippy>
+                    <img src="@/assets/icons/crown.svg" alt="best" />
+                </div>
+                <img src="@/assets/icons/Acura.svg" class="w-[88px] h-[56px] md:w-[150px] md:h-[112px]" alt="Acura" />
+            </div>
+            <div class="partner w-[158px] h-[110px] md:w-[228px] md:h-[176px] flex flex-col">
+                <div class="w-full flex justify-between md:justify-end px-[12px]">
+                    <Tippy theme="dark" :content="content">
+                        <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    </Tippy>
                 </div>
                 <img src="@/assets/icons/Eicher.svg" class="w-[88px] h-[56px] md:w-[150px] md:h-[112px]" alt="Eicher" />
             </div>
             <div class="partner w-[158px] h-[110px] md:w-[228px] md:h-[176px] flex flex-col">
                 <div class="w-full flex justify-between md:justify-end px-[12px]">
-                    <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    <Tippy theme="dark" :content="content">
+                        <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    </Tippy>
                 </div>
                 <img src="@/assets/icons/Nio.svg" class="w-[88px] h-[56px] md:w-[150px] md:h-[112px]" alt="Nio" />
             </div>
             <div class="partner w-[158px] h-[110px] md:w-[228px] md:h-[176px] flex flex-col">
                 <div class="w-full flex justify-between md:justify-end px-[12px]">
-                    <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    <Tippy theme="dark" :content="content">
+                        <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    </Tippy>
                 </div>
                 <img src="@/assets/icons/Englon.svg" class="w-[88px] h-[56px] md:w-[150px] md:h-[112px]" alt="Englon" />
             </div>
             <div class="partner w-[158px] h-[110px] md:w-[228px] md:h-[176px] flex flex-col">
                 <div class="w-full flex justify-between md:justify-end px-[12px]">
-                    <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    <Tippy theme="dark" :content="content">
+                        <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    </Tippy>
                 </div>
                 <img src="@/assets/icons/Englon.svg" class="w-[88px] h-[56px] md:w-[150px] md:h-[112px]" alt="Englon" />
             </div>
             <div class="partner w-[158px] h-[110px] md:w-[228px] md:h-[176px] flex flex-col">
                 <div class="w-full flex justify-between md:justify-end px-[12px]">
-                    <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    <Tippy theme="dark" :content="content">
+                        <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    </Tippy>
                 </div>
                 <img src="@/assets/icons/Englon.svg" class="w-[88px] h-[56px] md:w-[150px] md:h-[112px]" alt="Englon" />
             </div>
             <div class="partner w-[158px] h-[110px] md:w-[228px] md:h-[176px] flex flex-col">
                 <div class="w-full flex justify-between md:justify-end px-[12px]">
-                    <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    <Tippy theme="dark" :content="content">
+                        <img src="@/assets/icons/info.svg" class="md:hidden" alt="best" />
+                    </Tippy>
                 </div>
                 <img src="@/assets/icons/Englon.svg" class="w-[88px] h-[56px] md:w-[150px] md:h-[112px]" alt="Englon" />
             </div>
         </div>
         <div class="custom-container flex flex-col items-start my-[40px]">
             <p class="main--section--title">Наши Партнеры</p>
-            <img src="@/assets/img/partners-map.png" alt="logo" />
+            <img src="@/assets/img/partners-map.png" class="min-w-[328px] min-h-[166px]" alt="map" />
         </div>
     </div>
   </div>
@@ -81,7 +101,9 @@
 <script setup lang="ts">
 import Breadcrumbs from "@/components/ui/breadcrumbs.vue";
 import News from '@/components/main/News.vue';
-import { ref, onMounted } from 'vue';
+import { ref, onMounted, computed } from 'vue';
+import { Tippy } from 'vue-tippy'
+import 'tippy.js/dist/tippy.css' // optional for styling
 
 const scrollBtn = ref(null);
 
@@ -101,6 +123,11 @@ const scrollToTop = () => {
     behavior: 'smooth'
   });
 }
+const content = (title) => {
+    const div = document.createElement('div');
+    div.innerHTML = `<span class="font-medium">ACURA</span><br /><span class="text-[#CF9D52]">Адрес:</span><br />ул. Достоевского, д. 19<br /><span class="text-[#CF9D52]">Подарок</span><br />Купон на скидку 20%`;
+    return div;
+};
 </script>
 
 <style scoped>
