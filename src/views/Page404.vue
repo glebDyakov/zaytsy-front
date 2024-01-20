3ded9894212886ab92f05d4fd46dd5d2bfcc1411
glebdyakov <glebdyakov2000@gmail.com>
2024-01-20 09:58:52 +0300

    dev: bind 404

--- src/views/Page404.vue
+++ src/views/Page404.vue
@@ -3,7 +3,7 @@
         <div>
             <p class="page404--title text-[#378A6F] text-[80px] font-medium mb-[14px]">Ошибка 404</p>
             <p class="page404--subtitle mb-[30px]">Ой, кажется все зайцы разбежались</p>
-            <button class="btn btn--minimized w-[225px] h-[54px] flex justify-center items-center whitespace-nowrap">На главную</button>
+            <button class="btn btn--minimized w-[225px] h-[54px] flex justify-center items-center whitespace-nowrap" @click="$router.push('/')">На главную</button>
         </div>
         <div>
             <div class="w-[525.77px] h-[525.96px] bg-[#6CC4A7] absolute z-[-1] translate-y-[38px] rotate-[15deg]" />
